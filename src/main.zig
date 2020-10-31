f5dc8fe5eef2ead139031668d4b299355000f71f
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-10-31 05:56:11 -0400

    Better documentation of test.

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,14 @@
-//A quick graphics initialization
 const psp = @import("Zig-PSP/src/psp/pspsdk.zig");
-pub const panic = psp.debug.panic;
 
 //STD Overrides!
-pub const os = @import("Zig-PSP/src/psp/pspos.zig");
-const powerscale = @import("powerscale.zig");
-
 pub const std = @import("std");
+pub const os = @import("Zig-PSP/src/psp/pspos.zig");
+
+const powerscale = @import("powerscale.zig");
 
 comptime {
     asm(psp.module_info("Zig PSP App", 0, 1, 0));
 }
-pub var blkA: [4096]u8 = undefined;
-pub var blkB: [4096]u8 = undefined;
 
 extern fn scePowerSetClockFrequency(pllfreq: c_int, cpufreq: c_int, busfreq: c_int) c_int;
 pub fn main() !void {
@@ -22,27 +18,39 @@ pub fn main() !void {
     powerscale.init();
     defer powerscale.deinit();
 
+    // Create a database
     var f = try std.fs.cwd().createFile("./test.csv", .{.truncate = true});
+    defer f.close();
+
 
     var i : u32 = 2;
     while(true) : (i += 1) {
+        // Start recording
         powerscale.startRecord();
 
-        var z : usize = 0;
+        // Use this expression to control the stress curve
         var times: usize = if(i < 100) 20 * (100/2 % 100) else if(i < 150) 20 * (i/2 % 100) else 20 * (20/2 % 100);
-        while(z < times) : (z += 1){
-            @setRuntimeSafety(false);   
-            var x = std.math.cos(@intToFloat(f64, z));
 
-            
-        }  
+        // Artificial benchmark
+        // Run Cosine `times` number of times on iterator `z`.
+        var z : usize = 0;
+        while(z < times) : (z += 1){
+            @setRuntimeSafety(false);
+            var x = std.math.cos(@intToFloat(f64, z));            
+        }
+
+        // End of recording
         if(20 * (i/2 % 100) == 0){
             psp.sceKernelExitGame();
         }
 
+        // Set frequency
         powerscale.endRecord();
 
+        // Give us ze data!
         try f.writer().print("{d},{}\n", .{powerscale.getUsage() * 100, psp.scePowerGetCpuClockFrequency()});
+
+        // Not necessarily important here - it's not a factor in this application.
         //_ = psp.sceDisplayWaitVblankStart();
     }
 }
--- src/powerscale.zig
+++ src/powerscale.zig
@@ -1,16 +1,27 @@
+/// This is just here for making a *.a file
 extern fn sceRtcGetTickResolution() u32;
 extern fn sceRtcGetCurrentTick(tick: *u64) c_int;
-pub extern fn scePowerSetClockFrequency(pllfreq: c_int, cpufreq: c_int, busfreq: c_int) c_int;
+extern fn scePowerSetClockFrequency(pllfreq: c_int, cpufreq: c_int, busfreq: c_int) c_int;
 
+/// Required variables
+var currentTime: u64 = 0; // What is the current time
+var tickRate: u32 = 0; // RTC tick rate
+var lastTick: u64 = 0; // Last tick
+var lastUsage: f64 = 0; // Last CPU Usage %
+var cpuFreq: c_int = 333; // Current CPU Frequency
+var cpuTargetUtil: f64 = 0.70; //Targeted utilization (0.7 = 70%)
 
-const std = @import("std");
-
-var cpuFreq: c_int = 333;
-
+/// Returns the CPU frequency
 pub fn getCpuFreq() c_int{
     return cpuFreq;
 }
 
+/// Returns the CPU Usage
+pub fn getUsage() f64 {
+    return lastUsage;
+}
+
+/// Sets the CPU frequency (internal)
 fn setCpuFreq(newFreq: c_int) void {
     if(newFreq <= 333){
         cpuFreq = newFreq;
@@ -21,54 +32,58 @@ fn setCpuFreq(newFreq: c_int) void {
     }
 }
 
-var currentTime: u64 = 0;
-var tickRate: u32 = 0;
-var cpuTargetUtil: f64 = 0.70;
-var lastTick: u64 = 0;
-var lastUsage: f64 = 0;
-
-pub fn init() void {
-    tickRate = sceRtcGetTickResolution();
-    _ = sceRtcGetCurrentTick(&currentTime);
-    setCpuFreq(16);
-}
-
-pub fn setCPUUsageTarget(target: f64) void {
+/// Sets the targeted utilization
+pub fn setUsageTarget(target: f64) void {
    cpuTargetUtil = target; 
 }
 
+// Initializes the powerscale module
+pub fn init() void {
+    tickRate = sceRtcGetTickResolution();
+    _ = sceRtcGetCurrentTick(&currentTime);
+    setCpuFreq(333);
+}
+
+/// Terminates the module and sets frequency to 333 mhz
 pub fn deinit() void {
     setCpuFreq(333); //Set this up just in case.
 }
 
+/// Starts a recording
 pub fn startRecord() void {
     _ = sceRtcGetCurrentTick(&lastTick);
 }
 
-pub fn getUsage() f64 {
-    return lastUsage;
-}
-
+/// Ends Recording
 pub fn endRecord() void {
-    @setRuntimeSafety(false);
+    // Get our tick
     _ = sceRtcGetCurrentTick(&currentTime);
-    tickRate = sceRtcGetTickResolution();
 
+    // Calculate delta ticks
     var delta = @intToFloat(f64, @intCast(i64, currentTime) - @intCast(i64, lastTick));
-    var timeTaken: f64 = delta / @intToFloat(f64, tickRate); //Time in seconds.
 
+    // Calculate total time taken in seconds
+    var timeTaken: f64 = delta / @intToFloat(f64, tickRate);
+
+    // Utilization = timeTaken / fpsTarget
     var timeUtil = timeTaken / (1 / 60.0);
+
+    // Difference from desired CPU utilization
     var utilDiff = timeUtil - cpuTargetUtil;
     lastUsage = timeUtil;
 
+    // Frequency = current frequency + (current frequency / 0.5) * difference in utilization.
     var newFreqCalc = @floatToInt(c_int, @intToFloat(f64, cpuFreq) + @intToFloat(f64, cpuFreq)/2.0 * utilDiff);
 
+    // Limit the end result
     if(newFreqCalc < 20){
         newFreqCalc = 20;
     }
     if(newFreqCalc > 333){
         newFreqCalc = 333;
     }
+
+    // Set the new frequency
     setCpuFreq(newFreqCalc);
 }
 
